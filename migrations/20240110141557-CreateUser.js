afcb378017ca678b78aed900c81e1333fe45f713
Carlos Silva <carlos.uva.silva@gmail.com>
2024-01-10 16:49:30 +0000

    simplify migration file

--- migrations/20240110141557-CreateUser.js
+++ migrations/20240110141557-CreateUser.js
@@ -1,23 +1,10 @@
 'use strict';
 
-var dbm;
-var type;
-var seed;
-
-/**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-};
-
 exports.up = function (db, callback) {
   const createTableQuery = `
     CREATE TABLE users (
       id SERIAL PRIMARY KEY,
+      username VARCHAR(255) NOT NULL UNIQUE,
       name VARCHAR(255) NOT NULL,
       location VARCHAR(255) NOT NULL,
       created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
@@ -30,8 +17,4 @@ exports.up = function (db, callback) {
 exports.down = function(db, callback) {
   const dropTableQuery = `DROP TABLE users;`;
   db.runSql(dropTableQuery, callback);
-};
-
-exports._meta = {
-  "version": 1
 };
