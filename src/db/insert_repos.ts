c4ec4429e0063d725139c0952542af71ac55b28a
Carlos Silva <carlos.uva.silva@gmail.com>
2024-01-19 13:47:25 +0000

    fix:  change the insert of programming languages to use named parameters

--- src/db/insert_repos.ts
+++ src/db/insert_repos.ts
@@ -7,25 +7,36 @@ export async function saveUserRepos(
   try {
     // Save user data to the database
     for (const language of languages) {
+      const programming_language_data = {
+        name: language,
+      }
+
       const insertLanguageQuery = `
           INSERT INTO programming_languages (name)
-          VALUES ($1)
+          VALUES ($/name/)
           ON CONFLICT (name)
           DO UPDATE SET name = EXCLUDED.name
           RETURNING id;
         `;
 
-      const languageResult = await db.one(insertLanguageQuery, [language]);
+      const languageResult = await db.one(
+        insertLanguageQuery,
+        programming_language_data
+      );
 
       // Link the user to the programming language
+      const user_language_data = {
+        userId: userId,
+        language_id: languageResult.id
+      }
       const insertUserLanguageQuery = `
           INSERT INTO user_languages (user_id, language_id)
-            VALUES ($1, $2)
+            VALUES ($/userId/, $/language_id/)
             ON CONFLICT (user_id, language_id)
             DO NOTHING;
         `;
 
-      await db.none(insertUserLanguageQuery, [userId, languageResult.id]);
+      await db.none(insertUserLanguageQuery, user_language_data);
     }
 
   } catch (error: any) {
